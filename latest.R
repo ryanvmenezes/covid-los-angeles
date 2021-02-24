88dc1c24f60769c2bd65360406b795f9528de937
ryanvmenezes <ryanvmenezes@gmail.com>
2021-02-24 15:44:04 -0800

    latest

--- latest.R
+++ latest.R
@@ -11,16 +11,35 @@ counts.21 = data %>%
     deaths = sum(new_deaths, na.rm = TRUE)
   ) %>% 
   arrange(desc(week)) %>% 
+  mutate(week = lag(week) - 1) %>%
   filter(year(week) == 2021) %>% 
-  mutate(week = lag(week) - 1) %>% 
-  tail(-1)
+  drop_na(week)
 
 counts.21
 
+counts.21 %>% write_csv('latest-counts.csv', na = '')
+
+icu = read_csv('latdata/cdph-hospital-patient-county-totals.csv')
+
+icu
+
+icu.21 = icu %>% 
+  group_by(date) %>% 
+  summarise(
+    hosp = sum(positive_patients) + sum(icu_positive_patients)
+  ) %>% 
+  mutate(week = floor_date(date, 'weeks', week_start = 7)) %>% 
+  arrange(date) %>% 
+  group_by(week) %>% 
+  summarise(hosp = last(hosp)) %>% 
+  arrange(desc(week)) %>% 
+  filter(year(week) == 2021)
+
+icu.21 %>% write_csv('latest-hospitalizations.csv',  na = '')  
+
 counts.21 %>% 
+  left_join(icu.21) %>% 
   pivot_longer(-week) %>% 
   ggplot(aes(week, value)) +
   geom_bar(stat = 'identity') +
   facet_wrap(. ~ name, scale = 'free_y')
-
-counts.21 %>% write_csv('latest-counts.csv', na = '')
