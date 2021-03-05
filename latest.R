5fd74ef197cb9b8066ae3a9fdec42bdc28e16741
ryanvmenezes <ryanvmenezes@gmail.com>
2021-03-04 19:05:32 -0800

    latest

--- latest.R
+++ latest.R
@@ -36,6 +36,64 @@ hosp.totals = hosp %>%
 
 hosp.totals %>% write_csv('hospital-totals.csv', na = '')
 
+hosp.totals %>% 
+  ggplot(aes(date, hosp)) +
+  geom_line() +
+  geom_point(
+    data = . %>% filter(date > '2020-07-01') %>% filter(hosp == min(hosp)),
+    aes(date, hosp),
+    color = 'red',
+    size = 3
+  ) +
+  geom_text(
+    data = . %>% filter(date > '2020-07-01') %>% filter(hosp == min(hosp)),
+    aes(date, hosp + 1200, label = glue::glue('{date}\n{hosp}')),
+    color = 'red'
+  ) +
+  labs(
+    title = 'Hospitalizations'
+  ) +
+  theme_minimal()
+
+death.7day %>% 
+  ggplot(aes(date, deaths)) +
+  geom_line() +
+  geom_point(
+    data = . %>% filter(date > '2020-07-01') %>% filter(deaths == min(deaths)),
+    aes(date, deaths),
+    color = 'red',
+    size = 3
+  ) +
+  geom_text(
+    data = . %>% filter(date > '2020-07-01') %>% filter(deaths == min(deaths)),
+    aes(date, deaths + 100, label = glue::glue('{date}\n{round(deaths)}')),
+    color = 'red'
+  ) +
+  labs(
+    title = 'Deaths'
+  ) +
+  theme_minimal()
+
+
+case.7day %>% 
+  ggplot(aes(date, cases)) +
+  geom_line() +
+  geom_point(
+    data = . %>% filter(date > '2020-07-01') %>% filter(cases == min(cases)),
+    aes(date, cases),
+    color = 'red',
+    size = 3
+  ) +
+  geom_text(
+    data = . %>% filter(date > '2020-07-01') %>% filter(cases == min(cases)),
+    aes(date, cases + 5000, label = glue::glue('{date}\n{round(cases)}')),
+    color = 'red'
+  ) +
+  labs(
+    title = 'cases'
+  ) +
+  theme_minimal()
+
 # 
 # counts.by.week = data %>% 
 #   mutate(week = floor_date(date, 'weeks', week_start = 1)) %>% 
